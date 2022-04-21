f7de81ea6c1a143ba4f3d2013af1f4ffc9451bbd
Sanjay Rawat <sanjay.rawat@saffrontech.net>
2022-04-21 15:54:32 +0530

    Feat : pascal_trangle

---
+++ pascal_trangle.js
@@ -0,0 +1,23 @@
+var generate = function(numRows) {
+    const output =[[1],[1,1]]
+    let count=1
+    
+    while(count<numRows-1){
+        const abc =[1,...getArray(output[count]),1]
+        output.push([...abc])
+        console.log(output[output.length-1])
+        count++;
+    }
+    return output
+    
+function getArray(arr){
+    temp=[]
+    for(let i=0;i<arr.length-1;i++){
+        temp.push(arr[i] + arr[i+1])
+    }
+    return temp
+}
+    
+};
+
+console.log(generate(5))
